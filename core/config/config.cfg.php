34ffbc0c6ae0746d2f64ec34dc83fdffc985bf20
Max <webmaster@myxotod.de>
2013-05-18 23:01:12 +0200

    Update config.cfg.php

--- core/config/config.cfg.php
+++ core/config/config.cfg.php
@@ -1,6 +1,6 @@
 <?php
-$config['db_host']		= "localhost";
-$config['db_username']	= "d0173456";
-$config['db_password']	= "h4w9zb5v57bo";
-$config['db_name']		= "d0173456";
+$config['db_host']		= "**********";
+$config['db_username']	= "**********";
+$config['db_password']	= "**********";
+$config['db_name']		= "**********";
 ?>
